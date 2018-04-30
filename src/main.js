324f18238e626ce89d87416267189e94b6fc12ee
Álvaro Andrade <alvaro@Alvaros-iMac.local>
2018-04-30 18:23:20 -0300

    add Kendo UI Grid

--- src/main.js
+++ src/main.js
@@ -5,9 +5,13 @@ import App from './App'
 
 import '@progress/kendo-ui'
 import { KendoButtonGroup, ButtonsInstaller } from '@progress/kendo-buttons-vue-wrapper'
+import { KendoGridColumn, KendoGrid, GridInstaller } from '@progress/kendo-grid-vue-wrapper'
+import { KendoDataSource, DataSourceInstaller } from '@progress/kendo-datasource-vue-wrapper'
 
 Vue.config.productionTip = false
 Vue.use(ButtonsInstaller)
+Vue.use(GridInstaller)
+Vue.use(DataSourceInstaller)
 
 /* eslint-disable no-new */
 new Vue({
@@ -15,6 +19,9 @@ new Vue({
   template: '<App/>',
   components: {
     App,
-    KendoButtonGroup
+    KendoButtonGroup,
+    KendoGrid,
+    KendoGridColumn,
+    KendoDataSource
   }
 })
